d96edaa4298684f883b2b847a04f7da79db50409
przemyslawkrepski <krepski80@gmail.com>
2023-12-18 00:07:46 +0100

    components and props

--- react-z/src/main.jsx
+++ react-z/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {Link} from "./components/Link.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const linkElement = (
+    <a href="https://zrozumiecreact.pl"
+       target="_blank"
+       rel="noopener noreferrer"
+    >
+        Strona kursu
+    </a>
+);
+
+
+const element =
+    <>
+        <h1>Nasza Strona</h1>
+        <main>
+            < Link text = 'sprawdz strone kursu' shouldOpenNewTab = {true}/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, dolores earum eligendi et facilis nobis
+                ratione. Ducimus quas quisquam veritatis.</p>
+           <Link  shouldOpenNewTab = {false} />
+        </main>
+    </>;
+ReactDOM.createRoot(document.getElementById('root')).render(element);
