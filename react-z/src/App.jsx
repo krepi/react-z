3490a07fb4063ca492e51c0bccdad727c5ae5adb
przemyslawkrepski <krepski80@gmail.com>
2023-12-18 11:47:15 +0100

    end of first module

--- react-z/src/App.jsx
+++ react-z/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {PersonInfo} from "./components/PersonInfo.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const jan = {
+        name: "Jan",
+        lastName: "Kowalski",
+        mail: "j.kow@gmail.com",
+        tel: "+48 888 000 111"
+    }
+    const janina = {
+        name: "Janina",
+        lastName: "Kowalska",
+        mail: "jasia.kow@gmail.com",
+        tel: "+48 888 100 111"
+    }
+    const karol = {
+        name: "Karol",
+        lastName: "Kowalski",
+        mail: "karol.kow@gmail.com",
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    }
+    return (
+        <>
+            <PersonInfo person={jan}/>
+            <br/>
+            <PersonInfo person={janina}/>
+            <br/>
+            <PersonInfo person={karol}/>
+        </>
+    )
 }
 
 export default App
--- react-z/src/main.jsx
+++ react-z/src/main.jsx
@@ -1,24 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import {Link} from "./components/Link.jsx";
+import App from "./App.jsx";
 
-const linkElement = (
-    <a href="https://zrozumiecreact.pl"
-       target="_blank"
-       rel="noopener noreferrer"
-    >
-        Strona kursu
-    </a>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+        <App/>
+
 );
-
-
-const element =
-    <>
-        <h1>Nasza Strona</h1>
-        <main>
-            < Link text = 'sprawdz strone kursu' shouldOpenNewTab = {true}/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, dolores earum eligendi et facilis nobis
-                ratione. Ducimus quas quisquam veritatis.</p>
-           <Link  shouldOpenNewTab = {false} />
-        </main>
-    </>;
-ReactDOM.createRoot(document.getElementById('root')).render(element);
